fd17810eb43aa97d9aa78eee42eaa5d258ce18ee
unknown <samuelabatneh20@gmail.com>
2023-07-03 21:14:34 +0300

    add all

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { SharedService } from '../shared.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  login:any = FormGroup;
 
+  constructor(private fb:FormBuilder, private router:Router, private sharedService:SharedService) { }
+
+
+  ngOnInit(): void {
+    this.login = this.fb.group({
+      username:["", [Validators.required, Validators.minLength(4)]],
+      password:["", [Validators.required, Validators.minLength(4)]]
+    })
+  }
+
+
+
+  loginSubmit(login:FormGroup) {
+    this.sharedService.login(this.login.value)
+      .subscribe((res:any)=>{
+        if (res['access']){
+          localStorage.setItem('token', res['access']);
+          this.router.navigate(["home"]);
+        }
+      }
+      )
+
+
+  }
+
+
+  goToSignup(){
+    this.router.navigate(['register']);
+  }
+
+
+
+  getClass(form:any, fieldname:any):string{
+    let classList="form-control ";
+
+    if (form.get(fieldname).invalid && form.get(fieldname).touched){
+     classList += "is-invalid"
+    } else{
+     classList += "is-valid"
+    }
+
+    return classList;
+    }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { SharedService } from '../shared.service';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +9,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  register:any = FormGroup;
+  user:any = [];
+  constructor(private fb:FormBuilder, private router:Router, private sharedService:SharedService) { }
+
+  ngOnInit(): void {
+    this.register = this.fb.group({
+      name:["", [Validators.required, Validators.minLength(4)]],
+      email:["", Validators.compose([Validators.required, Validators.email])],
+      username:["", [Validators.required, Validators.minLength(4)]],
+      password:["", [Validators.required, Validators.minLength(4)]],
+      repeatPassword:["", [Validators.required, Validators.minLength(4)]]
+    })
+
+  }
+
+  submitRegister(data:any){
+    if(data.password===data.repeatPassword){
+    this.sharedService.addCustomer(data)
+      .subscribe((res)=>{
+        if (res!=="The User Name Already Exist!"){
+        this.router.navigate(['login'])
+      }
+      else{
+        alert(res);
+      }
+      }
+     )
+    }
+    else{
+      console.log("password not mach");
+    }
+  }
+
+  gotToLogin(){
+    this.router.navigate(['login']);
+  }
+
+  getClass(form:any, fieldname:any):string{
+    let classList="form-control ";
+
+    if (form.get(fieldname).invalid && form.get(fieldname).touched){
+     classList += "is-invalid"
+    } else{
+     classList += "is-valid"
+    }
+
+    return classList;
+    }
 
 }
