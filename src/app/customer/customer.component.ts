dc25582bb04d0a9ff0868a7521a8c9dc2719fabd
unknown <samuelabatneh20@gmail.com>
2023-07-03 21:06:44 +0300

    add all

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SharedService } from '../shared.service';
 
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
   styleUrls: ['./customer.component.css']
 })
-export class CustomerComponent {
+export class CustomerComponent implements OnInit {
 
+  constructor(private service:SharedService) { }
+
+  ngOnInit(): void {
+  }
+
+  loggedIn(){
+    return this.service.loggedIn();
+  }
 }
