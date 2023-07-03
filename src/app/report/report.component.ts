23d88bbfe80a9d7d9051be4d32f3349f89698ec2
unknown <samuelabatneh20@gmail.com>
2023-07-03 21:35:22 +0300

    add all

--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { SharedService } from '../shared.service';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ShowJobComponent } from '../job/show-job/show-job.component';
 
 @Component({
   selector: 'app-report',
@@ -6,5 +10,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./report.component.css']
 })
 export class ReportComponent {
+  form:any = FormGroup;
+  job = ShowJobComponent.getId();
+  id = this.job.id;
+  poster = this.job.customer
+  check = false;
+  userId!:Number|String;
 
+
+  constructor(private sharedService:SharedService, private router:Router, private fb:FormBuilder) { }
+
+  ngOnInit(): void {
+    this.getInfo();
+  }
+
+
+  getInfo() {
+    this.sharedService.getCustomerByUsername(this.poster).subscribe((response)=>{
+      this.userId = response.id;
+        this.form = this.fb.group({
+          jobPoster:[this.userId],
+          job:[this.id],
+        })
+        this.check = true;
+    })
+  }
+
+  reportSubmit(data:any){
+    this.sharedService.report(this.form.getRawValue())
+      .subscribe((res)=>{
+        alert(res);
+      });
+  }
 }
